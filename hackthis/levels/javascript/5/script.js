b7518f518687375aff93ff05b919217385b457c0
Breno Polanski <breno.polanski@gmail.com>
2016-06-02 23:35:29 -0300

    Update script.js

--- hackthis/levels/javascript/5/script.js
+++ hackthis/levels/javascript/5/script.js
@@ -1,4 +1,4 @@
-// The code this in: https://hackthis-10af.kxcdn.com/files/js/min/extra_48d468a93b.js?1429997775
+// The code is: https://hackthis-10af.kxcdn.com/files/js/min/extra_48d468a93b.js?1429997775
 
 // return "hats"
 a=window.location.host+"";b=a.length;c=4+((5*10)*2);d=String.fromCharCode(c,-(41-Math.floor(1806/13)),Math.sqrt(b-2)*29,(b*8)-29);
